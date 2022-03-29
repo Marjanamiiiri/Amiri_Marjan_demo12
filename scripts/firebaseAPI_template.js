e8369df56afdbc239d6c6a2de1386c832ddd0135
Marjan Amiri <marjanamiri778@gmail.com>
2022-03-29 12:04:49 -0700

    finally

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyB6NCQHn9XgzCFoDd3TNMAHOvuwyVWHRk4",
+    authDomain: "fir-comp1800-d6ca6.firebaseapp.com",
+    projectId: "fir-comp1800-d6ca6",
+    storageBucket: "fir-comp1800-d6ca6.appspot.com",
+    messagingSenderId: "642242272689",
+    appId: "1:642242272689:web:9fbce0924defe558acb190",
+    measurementId: "G-Y8T1E4T3BQ"
 };
 
 //--------------------------------------------
